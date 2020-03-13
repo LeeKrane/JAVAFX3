d103e0ddc542555b1425fa0f6a803d9fcad173b7
LeeKrane <godfist.leekrane@gmail.com>
2020-03-13 01:31:12 +0100

    - solved Labor18

--- src/main/java/controllers/Lab18_Task3_Controller.java
+++ src/main/java/controllers/Lab18_Task3_Controller.java
@@ -1,5 +1,71 @@
 package controllers;
 
-public class Lab18_Task3_Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+public class Lab18_Task3_Controller implements Initializable {
+	@FXML
+	private Button button;
+	
+	@FXML
+	private RadioButton option1;
+	
+	@FXML
+	private RadioButton option2;
+	
+	@FXML
+	private RadioButton option3;
+	
+	@FXML
+	private CheckBox checkBox;
+	
+	@FXML
+	private ListView<LogEntry> listView;
+	
+	@Override
+	public void initialize (URL url, ResourceBundle resourceBundle) {
+		EventHandler<ActionEvent> actionEventHandler = actionEvent -> {
+			Node node = (Node) actionEvent.getTarget();
+			if (node.toString().contains("RadioButton"))
+				listView.getItems().add(new LogEntry(((RadioButton)node).getText() + " selected", LocalDateTime.now()));
+			else if (node.toString().contains("Button"))
+				listView.getItems().add(new LogEntry("Button pressed", LocalDateTime.now()));
+			else
+				listView.getItems().add(new LogEntry("CheckBox " + (((CheckBox)node).isSelected() ? "" : "un") + "checked", LocalDateTime.now()));
+		};
+		
+		button.setOnAction(actionEventHandler);
+		option1.setOnAction(actionEventHandler);
+		option2.setOnAction(actionEventHandler);
+		option3.setOnAction(actionEventHandler);
+		checkBox.setOnAction(actionEventHandler);
+	}
+}
+
+class LogEntry {
+	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.uuuu HH:mm:ss");
+	private final String description;
+	private final LocalDateTime timestamp;
+	
+	LogEntry (String description, LocalDateTime timestamp) {
+		this.description = description;
+		this.timestamp = timestamp;
+	}
+	
+	@Override
+	public String toString () {
+		return timestamp.format(formatter) + ": " + description;
+	}
 }
--- src/main/java/launchers/Lab18_Launcher.java
+++ src/main/java/launchers/Lab18_Launcher.java
@@ -8,7 +8,7 @@ import javafx.application.Application;
 public class Lab18_Launcher {
 	public static void main (String[] args) {
 		//Application.launch(Lab18_Task1_ListDemo.class, args);
-		Application.launch(Lab18_Task2_NumberList.class, args);
-		//Application.launch(Lab18_Task3_Logger.class, args);
+		//Application.launch(Lab18_Task2_NumberList.class, args);
+		Application.launch(Lab18_Task3_Logger.class, args);
 	}
 }
